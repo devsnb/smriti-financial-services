b240f5f1cf65aab2c2eeff73170e8e8279fff829
Snehangshu Biswas <snehangshu@devsnb.com>
2022-10-23 10:55:28 +0530

    first working prototype

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+// Hamburger Menu Handler
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active')
+	navMenu.classList.toggle('active')
+})
+
+document.querySelectorAll('.nav-link').forEach(n =>
+	n.addEventListener('click', () => {
+		hamburger.classList.remove('active')
+		navMenu.classList.remove('active')
+	})
+)
+
+// Contact Us Form Handler
+const form = document.getElementById('main-form')
+
+const nameEl = document.getElementById('name')
+const contactNumberEl = document.getElementById('contact-number')
+const emailEl = document.getElementById('email')
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+
+	const data = {
+		name: nameEl.value,
+		contactNumber: contactNumberEl.value,
+		email: emailEl.value
+	}
+
+	console.log(data)
+})
